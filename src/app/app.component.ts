5cda0cd51bb82fc33472afbc8b51f9056b9a89be
Ted Wei <jdwei@hotmail.com>
2016-12-26 10:23:50 +0800

    Create a Menu with Active Page Highlight

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ export class MyApp {
   @ViewChild(Nav) nav: Nav;
 
   rootPage: any = Library;
+  activePage: any;
 
   pages: Array<{title: string, component: any}>;
 
@@ -26,6 +27,8 @@ export class MyApp {
       // { title: 'Page Two', component: Page2 },
       { title: 'Test DB Page', component: TestDbPage }
     ];
+
+    this.activePage = this.pages[0];
 
   }
 
@@ -42,5 +45,10 @@ export class MyApp {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
+    this.activePage = page;
+  }
+
+  checkActive(page) {
+    return page == this.activePage;
   }
 }
