f40d11ac6555db80e5279d5a6fea7361431d0672
Ted Wei <jdwei@hotmail.com>
2017-01-19 06:03:25 +0800

    createTestDbItems

--- src/providers/library-service.ts
+++ src/providers/library-service.ts
@@ -13,49 +13,51 @@ export class LibraryService {
   }
 
   syncToServer(list: LibraryData[]): Promise<any> {
+    return Promise.all([this.deleteAllTestDbItems(), this.createTestDbItems(list)]);
+  }
+
+  syncToClient() {
+
+  }
+
+  private deleteAllTestDbItems() {
     let self = this
     var promise = new Promise(function(resolve, reject) {
-      // resolve();
-      self.http.get('https://www.reddit.com/r/gifs/new/.json?limit=10').map(res => res.json()).subscribe(data => {
-        console.log("data2 : " + JSON.stringify(data))
+      var url = self.SERVER_URL + "/deleteAllTestDbItems";
+      self.http.get(url).map(res => res.json()).subscribe(data => {
+        console.log("deleteAllTestDbItems : " + JSON.stringify(data))
         resolve(data)
       }, error => {
         console.log("error : " + JSON.stringify(error))
         reject(error)
       });
-      // // do a thing, possibly async, then…
-
-      // if (/* everything turned out fine */) {
-      //   resolve("Stuff worked!");
-      // }
-      // else {
-      //   reject(Error("It broke"));
-      // }
     });
     return promise;
-
-
-    // this.http.get('http://better-computer.com/testservice/Service2.svc/getAllTestDbItems').subscribe(data => {
-    //     console.log("data3 : " + data)
-    // });
-
-    // var postData = [];
-    // for (let item of list) {
-    //   postData.push({
-    //     cat: item.cat,
-    //     subcat: item.subcat,
-    //     item: item.item,
-    //     dialog: item.dialog
-    //   })
-    // }
-    // let strPostData = JSON.stringify(postData)
-    // console.log("strPostData : " + strPostData)
-    // this.http.post(this.SERVER_URL + "/createTestDbItems", strPostData).subscribe(data => {
-    //     console.log("data : " + data)
-    // });
   }
 
-  syncToClient() {
-
+  private createTestDbItems(list: LibraryData[]): Promise<any> {
+    let self = this
+    var promise = new Promise(function(resolve, reject) {
+      var postData = [];
+      for (let item of list) {
+        postData.push({
+          cat: item.cat,
+          subcat: item.subcat,
+          item: item.item,
+          dialog: item.dialog
+        })
+      }
+      let strPostData = JSON.stringify(postData)
+      console.log("strPostData : " + strPostData)
+      var url = self.SERVER_URL + "/createTestDbItems";
+      self.http.post(url, strPostData).map(res => res.json()).subscribe(data => {
+        console.log("createTestDbItems : " + JSON.stringify(data))
+        resolve(data)
+      }, error => {
+        console.log("error : " + JSON.stringify(error))
+        reject(error)
+      });
+    });
+    return promise;
   }
 }
